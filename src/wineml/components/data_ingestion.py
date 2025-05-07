668c9eb009650a323ac2679deab2bcc78019c96e
amahbub2006 <azfarmahbub@gmail.com>
2025-05-07 19:59:28 -0400

    final commit for now

--- src/wineml/components/data_ingestion.py
+++ src/wineml/components/data_ingestion.py
@@ -32,7 +32,8 @@ class DataIngestion:
         os.makedirs(unzip_path, exist_ok=True)
 
         with zipfile.ZipFile(self.config.local_data_file, 'r') as zip_ref:
-            zip_ref.extractall(unzip_path)
+            zip_ref.extractall(
+                unzip_path)
 
         logger.info(f"Data extracted to: {unzip_path}")
 
